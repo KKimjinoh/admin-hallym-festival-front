9d10f0cb03a4ff9e87197c8fdb5511fdf36a36af
Gi_Hoon <kiki01111@naver.com>
2024-05-19 03:37:05 +0900

    Feat:커뮤니티 ui수정2

--- src/components/Modal/Community/CommuDelete.jsx
+++ src/components/Modal/Community/CommuDelete.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./CommuDelete.scss";
 import { deleteCommunityDetail } from "../../../apis/axios";
+
 const CommuDelete = ({ id, closeModal }) => {
   const deleteArticle = async () => {
     try {
-      //delete메소드 성공 여부, test일 경우 undefined
+      //delete 메소드 성공 여부, test일 경우 undefined
       const result = await deleteCommunityDetail(id);
       console.log(result);
-      //delete완료시 모달 닫음
+      //delete 완료시 모달 닫음
       if (result) {
         closeModal();
         window.location.reload();
@@ -18,15 +19,18 @@ const CommuDelete = ({ id, closeModal }) => {
   };
 
   return (
-    <div className="delete" onClick={(e) => e.stopPropagation()}>
-      <div className="delete-body">
-        댓글을 삭제하시면 다시 되돌릴 수 없습니다.
-      </div>
-      <div className="delete-btn" onClick={() => deleteArticle()}>
-        삭제하기
+    <div className="delete-overlay" onClick={closeModal}>
+      <div className="delete" onClick={(e) => e.stopPropagation()}>
+        <div className="delete-body">
+          <div className="delete-body-title">삭제하시겠습니까?</div>
+          <p>삭제하시면 다시 되돌릴 수 없습니다.</p>
+        </div>
+        <div className="delete-btn" onClick={() => deleteArticle()}>
+          삭제하기
+        </div>
       </div>
     </div>
   );
 };
-//페스워드 입력받음 입력받은 패스워드랑 id 보내고 id에 해당하는 패스워드랑 같은지 확인
+
 export default CommuDelete;
